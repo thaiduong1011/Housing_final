f08e7e41cd8ce6a3b1c2ed0e534b28747d7424de
thaiduong1011 <36438062+thaiduong1011@users.noreply.github.com>
2018-04-15 22:56:08 +0700

    search_fragment

--- app/src/main/java/android/demo/housing_yuong/SearchFragment.java
+++ app/src/main/java/android/demo/housing_yuong/SearchFragment.java
@@ -13,6 +13,6 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_noti, container, false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
