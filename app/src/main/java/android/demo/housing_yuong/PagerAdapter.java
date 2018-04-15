c27e25e412708954da3cf7fae94546a8b810fb82
thaiduong1011 <36438062+thaiduong1011@users.noreply.github.com>
2018-04-15 11:17:54 +0700

    file test

---
+++ app/src/main/java/android/demo/housing_yuong/PagerAdapter.java
@@ -0,0 +1,73 @@
+package android.demo.housing_yuong;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PagerAdapter  extends FragmentPagerAdapter {
+    private final List<Fragment> mFragments = new ArrayList<>();
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragments.add(fragment);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+    /*public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        Fragment frag=null;
+        switch (position){
+            case 0:
+                frag=new AndroidFragment();
+                break;
+            case 1:
+                frag=new IOSFragment();
+                break;
+            case 2:
+                frag=new PHPFragment();
+                break;
+        }
+        return frag;
+    }
+    @Override
+    public int getCount() {
+        return 3;
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = "";
+        switch (position){
+            case 0:
+                title="Android";
+                break;
+            case 1:
+                title="IOS";
+                break;
+            case 2:
+                title="PHP";
+                break;
+        }
+
+        return title;
+    }*/
+
+}
---
+++ app/src/main/java/android/demo/housing_yuong/SearchFragment.java
@@ -0,0 +1,18 @@
+package android.demo.housing_yuong;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+    public SearchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_noti, container, false);
+    }
+}
